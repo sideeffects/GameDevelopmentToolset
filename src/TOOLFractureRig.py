c68f0dee192054e9d68ccd071b90f230b13cbbf1
Steven Burrichter <steveb@stevencburrichter.com>
2016-01-18 17:02:43 -0500

    Fracture tool now supports processing multiple RBD Packed Objects! Woo!

--- src/TOOLFractureRig.py
+++ src/TOOLFractureRig.py
@@ -22,11 +22,11 @@ class FRACTURE_RIG:
     ######################################
     #VARIABLES
     ######################################
-    numberOfPieces  =   0
+    numberOfPieces  =   []
     nodeGeo         =   None
     nodeSubnet      =   None
     nodePieces      =   []
-    nodeDisplay     =   None
+    nodeDisplay     =   []
     PARMS           =   ["tx", "ty", "tz", "rx", "ry", "rz", "px", "py", "pz"]
 
     ######################################
@@ -41,12 +41,15 @@ class FRACTURE_RIG:
         self.nodeGeo = hou.selectedNodes()
 
         if len(self.nodeGeo) > 1:
-            return False
+            #Multiple assets
+            #TODO: Check all nodes are the same before moving forward
+            node_type = self.nodeGeo[0].type()
         elif len(self.nodeGeo) < 1:
             return False
         else:
             node_type = self.nodeGeo[0].type()
 
+        #TODO: Solve for multiple nodes
         if self.nodeGeo[0].parm('soppath') == None:
             print "DOP Node doesn't contain a soppath!"
             return False
@@ -58,14 +61,16 @@ class FRACTURE_RIG:
     #################################
     def GrabPieces(self):
         self.nodeDisplay = self.FindReferenceGeometry()
-        obj_geometry = self.nodeDisplay.geometry()
 
-        if obj_geometry.findPointAttrib("name") == None:
-            list_pieces = obj_geometry.findPrimAttrib("name").strings()
-        else:
-            list_pieces = obj_geometry.findPointAttrib("name").strings()
+        for index in range(0, len(self.nodeDisplay)):
+            obj_geometry = self.nodeDisplay[index].geometry()
 
-        self.numberOfPieces = len(list_pieces)
+            if obj_geometry.findPointAttrib("name") == None:
+                list_pieces = obj_geometry.findPrimAttrib("name").strings()
+            else:
+                list_pieces = obj_geometry.findPointAttrib("name").strings()
+
+            self.numberOfPieces.append(len(list_pieces))
 
     #################################
     #Creates the subnet to contain
@@ -82,11 +87,18 @@ class FRACTURE_RIG:
     #for the pieces
     #################################
     def GenerateGeo(self):
-        for index in range(0, self.numberOfPieces):
-            if(hou.node("/obj/FBX_RESULT/PIECE" + str(index)) == None):
-                self.nodePieces.insert(index, self.nodeSubnet.createNode("geo", "PIECE" + str(index)))
-            else:
-                self.nodePieces.insert(index, hou.node("/obj/FBX_RESULT/PIECE" + str(index)))
+        for objectToProcess in range(0, len(self.nodeDisplay)):
+            self.nodePieces.append([])
+
+            for index in range(0, self.numberOfPieces[objectToProcess]):
+                #TODO: Append name to each geo node
+                tempName = self.nodeDisplay[objectToProcess].parent().name()
+                print tempName
+
+                if(hou.node("/obj/FBX_RESULT/" + tempName + "_PIECE" + str(index)) == None):
+                    self.nodePieces[objectToProcess].insert(index, self.nodeSubnet.createNode("geo", tempName + "_PIECE" + str(index)))
+                else:
+                    self.nodePieces[objectToProcess].insert(index, hou.node("/obj/FBX_RESULT/" + tempName + "_PIECE" + str(index)))
 
     #################################
     #Crawls the original geometry to
@@ -94,20 +106,27 @@ class FRACTURE_RIG:
     #for piece calculations
     #################################
     def FindReferenceGeometry(self):
-        temp = self.nodeGeo[0].parm('soppath').unexpandedString()
-        if 'opinputpath' in temp:
-            temp = temp.split('"')[1::2][0]
-        temp = self.nodeGeo[0].node(temp)
+        displayNodes = []
 
-        referenceFound = False
+        for index in range(0, len(self.nodeGeo)):
+            temp = self.nodeGeo[index].parm('soppath').unexpandedString()
+            if 'opinputpath' in temp:
+                temp = temp.split('"')[1::2][0]
+            temp = self.nodeGeo[index].node(temp)
 
-        while(referenceFound == False):
-            if(temp.geometry().findPrimAttrib('dopobject') != None):
-                temp = temp.inputs()[0]
-            elif(temp.geometry() == None):
-                temp = temp.inputs()[0]
-            else:
-                return temp
+            referenceFound = False
+
+            while(referenceFound == False):
+                if(temp.geometry().findPrimAttrib('dopobject') != None):
+                    temp = temp.inputs()[0]
+                elif(temp.geometry() == None):
+                    temp = temp.inputs()[0]
+                else:
+                    # return temp
+                    displayNodes.append(temp)
+                    break
+
+        return displayNodes
 
     #################################
     #Modifies the created piece geo
@@ -115,112 +134,120 @@ class FRACTURE_RIG:
     #with custom expressions.
     #################################
     def ModifyGeo(self):
-        refGeo = self.FindReferenceGeometry()
-        unpack = None
-        deleteUnpack = None
+        for objectToProcess in range(0, len(self.nodeDisplay)):
+            # refGeo = self.FindReferenceGeometry()
+            refGeo = self.nodeDisplay[objectToProcess]
+            unpack = None
+            deleteUnpack = None
 
-        if(refGeo.parent().node("UNPACK")  == None):
-            unpack = refGeo.parent().createNode("unpack", "UNPACK")
-        elif (refGeo.parent().node("UNPACK")  != None):
-            unpack = refGeo.parent().node("UNPACK")
-        else:
-            print "ERROR 0: Cannot generate unpack node!"
+            tempName = self.nodeDisplay[objectToProcess].parent().name()
 
-        unpack.setFirstInput(refGeo)
-
-        for index in range(0, self.numberOfPieces):
-            proxy   = None
-            delete  = None
-            xform   = None
-
-            #################################
-            #Create the nodes to process the
-            #individual pieces
-            #################################
-            if(hou.node("/obj/FBX_RESULT/PIECE" + str(index) + "/PROXY")  == None):
-                proxy = self.nodePieces[index].createNode("object_merge", "PROXY")
-            elif (hou.node("/obj/FBX_RESULT/PIECE" + str(index) + "/PROXY")  != None):
-                proxy = hou.node("/obj/FBX_RESULT/PIECE" + str(index) + "/PROXY")
+            if(refGeo.parent().node("UNPACK")  == None):
+                unpack = refGeo.parent().createNode("unpack", "UNPACK")
+            elif (refGeo.parent().node("UNPACK")  != None):
+                unpack = refGeo.parent().node("UNPACK")
             else:
-                print "ERROR 0: Cannot generate proxy node!"
+                print "ERROR 0: Cannot generate unpack node!"
 
-            proxy.setParms({"objpath1":proxy.relativePathTo(unpack),
-                            "xformtype":"object",
-                            "xformpath":"../.."})
+            unpack.setFirstInput(refGeo)
 
-            if(hou.node("/obj/FBX_RESULT/PIECE" + str(index) + "/DELETE")  == None):
-                delete = self.nodePieces[index].createNode("delete", "DELETE")
-            elif (hou.node("/obj/FBX_RESULT/PIECE" + str(index) + "/DELETE")  != None):
-                delete = hou.node("/obj/FBX_RESULT/PIECE" + str(index) + "/DELETE")
-            else:
-                print "ERROR 0: Cannot generate proxy node!"
+            for index in range(0, self.numberOfPieces[objectToProcess]):
+                proxy   = None
+                delete  = None
+                xform   = None
 
-            delete.setParms({'group':"@name=piece" + str(index),
-                            'negate':'keep'})
-            delete.setFirstInput(proxy)
+                print self.nodePieces[objectToProcess][index]
 
-            if(hou.node("/obj/FBX_RESULT/PIECE" + str(index) + "/XFORM")  == None):
-                xform = self.nodePieces[index].createNode("xform", "XFORM")
-            elif (hou.node("/obj/FBX_RESULT/PIECE" + str(index) + "/XFORM")  != None):
-                xform = hou.node("/obj/FBX_RESULT/PIECE" + str(index) + "/XFORM")
-            else:
-                print "ERROR 0: Cannot generate proxy node!"
+                #TODO: Figure out order of operations
+                #################################
+                #Create the nodes to process the
+                #individual pieces
+                #################################
+                if(hou.node("/obj/FBX_RESULT/" + tempName + "_PIECE" + str(index) + "/PROXY")  == None):
+                    proxy = self.nodePieces[objectToProcess][index].createNode("object_merge", "PROXY")
+                elif (hou.node("/obj/FBX_RESULT/" + tempName + "_PIECE" + str(index) + "/PROXY")  != None):
+                    proxy = hou.node("/obj/FBX_RESULT/" + tempName + "_PIECE" + str(index) + "/PROXY")
+                else:
+                    print "ERROR 0: Cannot generate proxy node!"
+
+                proxy.setParms({"objpath1":proxy.relativePathTo(unpack),
+                                "xformtype":"object",
+                                "xformpath":"../.."})
+
+                if(hou.node("/obj/FBX_RESULT/" + tempName + "_PIECE" + str(index) + "/DELETE")  == None):
+                    delete = self.nodePieces[objectToProcess][index].createNode("delete", "DELETE")
+                elif (hou.node("/obj/FBX_RESULT/" + tempName + "_PIECE" + str(index) + "/DELETE")  != None):
+                    delete = hou.node("/obj/FBX_RESULT/" + tempName + "_PIECE" + str(index) + "/DELETE")
+                else:
+                    print "ERROR 0: Cannot generate proxy node!"
+
+                delete.setParms({'group':"@name=piece" + str(index),
+                                'negate':'keep'})
+                delete.setFirstInput(proxy)
+
+                if(hou.node("/obj/FBX_RESULT/" + tempName + "_PIECE" + str(index) + "/XFORM")  == None):
+                    xform = self.nodePieces[objectToProcess][index].createNode("xform", "XFORM")
+                elif (hou.node("/obj/FBX_RESULT/" + tempName + "_PIECE" + str(index) + "/XFORM")  != None):
+                    xform = hou.node("/obj/FBX_RESULT/" + tempName + "_PIECE" + str(index) + "/XFORM")
+                else:
+                    print "ERROR 0: Cannot generate proxy node!"
 
 
-            #################################
-            #This is important to grab the correct transform for the pieces.
-            #Otherwise, all transforms will be at [0,0,0] and ruin your day.
-            #The key here is finding the delta of the rest values between the
-            #creation frame and the next frame.
-            #################################
-            xform.setFirstInput(delete)
-            xform.parm('movecentroid').pressButton()
+                #################################
+                #This is important to grab the correct transform for the pieces.
+                #Otherwise, all transforms will be at [0,0,0] and ruin your day.
+                #The key here is finding the delta of the rest values between the
+                #creation frame and the next frame.
+                #################################
+                xform.setFirstInput(delete)
+                xform.parm('movecentroid').pressButton()
 
-            creationFrame = self.nodeGeo[0].parm('createframe').eval()
-            hou.setFrame(creationFrame)
-            dopxform1 = self.nodeGeo[0].simulation().findObject(self.nodeGeo[0].name()).geometry().iterPoints()[index].attribValue('rest')
-            hou.setFrame(creationFrame+1)
-            dopxform2 = self.nodeGeo[0].simulation().findObject(self.nodeGeo[0].name()).geometry().iterPoints()[index].attribValue('rest')
-            deltaXFORM = []
-            hou.setFrame(creationFrame)
+                #TODO: From event. Figure out calculations.
+                creationFrame = self.nodeGeo[objectToProcess].parm('createframe').eval()
+                hou.setFrame(creationFrame)
+                dopxform1 = self.nodeGeo[objectToProcess].simulation().findObject(self.nodeGeo[objectToProcess].name()).geometry().iterPoints()[index].attribValue('rest')
+                hou.setFrame(creationFrame+1)
+                dopxform2 = self.nodeGeo[objectToProcess].simulation().findObject(self.nodeGeo[objectToProcess].name()).geometry().iterPoints()[index].attribValue('rest')
+                deltaXFORM = []
+                hou.setFrame(creationFrame)
 
-            for i in range(0,3):
-                deltaXFORM.insert(i, dopxform1[i]-dopxform2[i])
+                for i in range(0,3):
+                    deltaXFORM.insert(i, dopxform1[i]-dopxform2[i])
 
-            translates = [xform.parm('tx').eval(), xform.parm('ty').eval(), xform.parm('tz').eval()]
-            result = [translates[0]+deltaXFORM[0], translates[1]+deltaXFORM[1], translates[2]+deltaXFORM[2]]
-            result = []
-            for i in range(0,3):
-                result.insert(i, translates[i]+deltaXFORM[i])
+                translates = [xform.parm('tx').eval(), xform.parm('ty').eval(), xform.parm('tz').eval()]
+                result = [translates[0]+deltaXFORM[0], translates[1]+deltaXFORM[1], translates[2]+deltaXFORM[2]]
+                result = []
+                for i in range(0,3):
+                    result.insert(i, translates[i]+deltaXFORM[i])
 
-            xform.setParms({'tx':result[0],
-                            'ty':result[1],
-                            'tz':result[2]})
+                xform.setParms({'tx':result[0],
+                                'ty':result[1],
+                                'tz':result[2]})
 
-            #################################
-            #File I/O. Saves a temp obj to reset the mesh/transform
-            #################################
-            # if(hou.node("/obj/FBX_RESULT/PIECE" + str(index) + "/FILE")  == None):
-            #     file = self.nodePieces[index].createNode("file", "FILE")
-            # elif (hou.node("/obj/FBX_RESULT/PIECE" + str(index) + "/FILE")  != None):
-            #     file = hou.node("/obj/FBX_RESULT/PIECE" + str(index) + "/FILE")
-            # else:
-            #     print "ERROR 0: Cannot generate file node!"
-            #
-            # file.setParms({'filemode':'auto',
-            #                 'file':'$HIP/tmp/temp_piece_' + str(index) + '.bgeo'})
-            # file.setFirstInput(xform)
+                #################################
+                #File I/O. Saves a temp obj to reset the mesh/transform
+                #################################
+                # if(hou.node("/obj/FBX_RESULT/PIECE" + str(index) + "/FILE")  == None):
+                #     file = self.nodePieces[index].createNode("file", "FILE")
+                # elif (hou.node("/obj/FBX_RESULT/PIECE" + str(index) + "/FILE")  != None):
+                #     file = hou.node("/obj/FBX_RESULT/PIECE" + str(index) + "/FILE")
+                # else:
+                #     print "ERROR 0: Cannot generate file node!"
+                #
+                # file.setParms({'filemode':'auto',
+                #                 'file':'$HIP/tmp/temp_piece_' + str(index) + '.bgeo'})
+                # file.setFirstInput(xform)
 
-            #################################
-            #Delete original file node
-            #################################
-            if(str(hou.node("/obj/FBX_RESULT/PIECE" + str(index) + "/file1")) != "None"):
-                hou.node("/obj/FBX_RESULT/PIECE" + str(index) + "/file1").destroy()
+                #################################
+                #Delete original file node
+                #################################
+                if(str(hou.node("/obj/FBX_RESULT/" + tempName + "_PIECE" + str(index) + "/file1")) != "None"):
+                    hou.node("/obj/FBX_RESULT/" + tempName + "_PIECE" + str(index) + "/file1").destroy()
 
-            self.ArrangeAllNodes(self.nodePieces[index].path())
+                self.ArrangeAllNodes(self.nodePieces[objectToProcess][index].path())
 
-        self.ArrangeAllNodes("/obj")
-        self.ArrangeAllNodes(self.nodeSubnet.path())
+            self.ArrangeAllNodes("/obj")
+            self.ArrangeAllNodes(self.nodeSubnet.path())
 
 
     #################################
@@ -241,22 +268,23 @@ class FRACTURE_RIG:
 
         for frame in range(RFSTART, RFEND+1):
             hou.setFrame(frame)
-            print "Processing Frame: " + str(frame)
+            # print "Processing Frame: " + str(frame)
 
-            for index in range(0,self.numberOfPieces):
-                for index_parm in range(0,3):
-                    hou_keyed_parm = self.nodePieces[index].parm(self.PARMS[index_parm])
-                    hou_keyframe = hou.Keyframe()
-                    hou_keyframe.setFrame(frame)
-                    hou_keyframe.setValue(self.nodeGeo[0].simulation().findObject(self.nodeGeo[0].name()).geometry().iterPoints()[index].attribValue('P')[index_parm])
-                    hou_keyed_parm.setKeyframe(hou_keyframe)
+            for objectToProcess in range(0, len(self.numberOfPieces)):
+                for index in range(0,self.numberOfPieces[objectToProcess]):
+                    for index_parm in range(0,3):
+                        hou_keyed_parm = self.nodePieces[objectToProcess][index].parm(self.PARMS[index_parm])
+                        hou_keyframe = hou.Keyframe()
+                        hou_keyframe.setFrame(frame)
+                        hou_keyframe.setValue(self.nodeGeo[objectToProcess].simulation().findObject(self.nodeGeo[objectToProcess].name()).geometry().iterPoints()[index].attribValue('P')[index_parm])
+                        hou_keyed_parm.setKeyframe(hou_keyframe)
 
-                for index_parm in range(0,3):
-                    hou_keyed_parm = self.nodePieces[index].parm(self.PARMS[index_parm+3])
-                    hou_keyframe = hou.Keyframe()
-                    hou_keyframe.setFrame(frame)
-                    hou_keyframe.setValue(hou.Quaternion(self.nodeGeo[0].simulation().findObject(self.nodeGeo[0].name()).geometry().iterPoints()[index].attribValue('orient')).extractEulerRotates()[index_parm])
-                    hou_keyed_parm.setKeyframe(hou_keyframe)
+                    for index_parm in range(0,3):
+                        hou_keyed_parm = self.nodePieces[objectToProcess][index].parm(self.PARMS[index_parm+3])
+                        hou_keyframe = hou.Keyframe()
+                        hou_keyframe.setFrame(frame)
+                        hou_keyframe.setValue(hou.Quaternion(self.nodeGeo[objectToProcess].simulation().findObject(self.nodeGeo[objectToProcess].name()).geometry().iterPoints()[index].attribValue('orient')).extractEulerRotates()[index_parm])
+                        hou_keyed_parm.setKeyframe(hou_keyframe)
 
         print "Processing Complete!"
 

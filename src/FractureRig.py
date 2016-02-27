c7e6e738a59de600374d720b42b7b17f08720ba6
Steven Burrichter <steveb@stevencburrichter.com>
2016-02-26 17:35:23 -0800

    Made the tool a little more pythonic using the format string method.

--- src/FractureRig.py
+++ src/FractureRig.py
@@ -8,13 +8,13 @@ reload(RealTimeVFXToolset)
 
 def init(nodes):
     if checkSelections(nodes) == False:
-        return "Tool failed! See above."
+        return 'Tool failed! See above.'
 
     geometryNodes = RealTimeVFXToolset.findNonDOPGeometry(nodes, 'soppath', 'dopobject')
     numberOfPieces = RealTimeVFXToolset.indexAttributeEntries(geometryNodes, 'name')
 
     if checkNaming(geometryNodes) == False:
-        return "Tool failed! See above."
+        return 'Tool failed! See above.'
 
     rootNode = hou.node('/obj').createNode('subnet', 'FBX_RESULT')
     nodePieces = generateGeometry(geometryNodes, numberOfPieces, rootNode)
@@ -38,10 +38,10 @@ def generateGeometry(geoNodes, numPieces, root):
         for index in range(0, numPieces[objectToProcess]):
             tempName = geoNodes[objectToProcess].parent().name()
 
-            if(hou.node(root.path() + tempName + "_PIECE" + str(index)) == None):
-                nodePieces[objectToProcess].insert(index, root.createNode("geo", tempName + "_PIECE" + str(index)))
+            if(hou.node('{}{}_PIECE{}'.format(root.path(), tempName, index)) == None):
+                nodePieces[objectToProcess].insert(index, root.createNode('geo', '{}_PIECE{}'.format(tempName, index)))
             else:
-                nodePieces[objectToProcess].insert(index, hou.node(root.path() + tempName + "_PIECE" + str(index)))
+                nodePieces[objectToProcess].insert(index, hou.node('{}{}_PIECE{}'.format(root.path(), tempName, index)))
 
         nodePieces[objectToProcess][0].parent().layoutChildren()
 
@@ -55,12 +55,14 @@ def modifyGeo(nodes, geometryNodes, nodePieces, numPieces, root):
 
         tempName = geometryNodes[objectToProcess].parent().name()
 
-        if(refGeo.parent().node("UNPACK" + str(objectToProcess))  == None):
-            unpack = refGeo.parent().createNode("unpack", "UNPACK" + str(objectToProcess))
-        elif (refGeo.parent().node("UNPACK" + str(objectToProcess))  != None):
-            unpack = refGeo.parent().node("UNPACK" + str(objectToProcess))
+        stringInput = 'UNPACK{}'.format(objectToProcess)
+        
+        if(refGeo.parent().node(stringInput)  == None):
+            unpack = refGeo.parent().createNode('unpack', stringInput)
+        elif (refGeo.parent().node(stringInput)  != None):
+            unpack = refGeo.parent().node(stringInput)
         else:
-            print "ERROR 0: Cannot generate unpack node!"
+            print 'ERROR 0: Cannot generate unpack node!'
 
         unpack.setFirstInput(refGeo)
 
@@ -80,17 +82,17 @@ def modifyGeo(nodes, geometryNodes, nodePieces, numPieces, root):
             #Create the nodes to process the
             #individual pieces
             #################################
-            proxy = nodePieces[objectToProcess][index].createNode("object_merge", "PROXY")
-            proxy.setParms({"objpath1":proxy.relativePathTo(unpack),
-                            "xformtype":"object",
-                            "xformpath":"../.."})
+            proxy = nodePieces[objectToProcess][index].createNode('object_merge', 'PROXY')
+            proxy.setParms({'objpath1':proxy.relativePathTo(unpack),
+                            'xformtype':'object',
+                            'xformpath':'../..'})
 
-            delete = nodePieces[objectToProcess][index].createNode("delete", "DELETE")
-            delete.setParms({'group':"@name=piece" + str(index),
+            delete = nodePieces[objectToProcess][index].createNode('delete', 'DELETE')
+            delete.setParms({'group':'@name=piece{}'.format(index),
                             'negate':'keep'})
             delete.setFirstInput(proxy)
 
-            xform = nodePieces[objectToProcess][index].createNode("xform", "XFORM")
+            xform = nodePieces[objectToProcess][index].createNode('xform', 'XFORM')
 
             #################################
             #This is important to grab the correct transform for the pieces.
@@ -128,17 +130,17 @@ def modifyGeo(nodes, geometryNodes, nodePieces, numPieces, root):
             #Delete original file node
             #################################
             if(xform.parent().node('file1') != None):
-                xform.parent().node("file1").destroy()
+                xform.parent().node('file1').destroy()
             xform.parent().layoutChildren()
 
 def processMesh(nodes, nodePieces, numPieces):
-    PARMS   =   ["tx", "ty", "tz", "rx", "ry", "rz", "px", "py", "pz"]
+    PARMS   =   ['tx', 'ty', 'tz', 'rx', 'ry', 'rz', 'px', 'py', 'pz']
     RFSTART = int(hou.expandString('$RFSTART'))
     RFEND = int(hou.expandString('$RFEND'))
 
     for frame in range(RFSTART, RFEND+1):
         hou.setFrame(frame)
-        print "Processing Frame: " + str(frame)
+        print 'Processing Frame: {}'.format(frame)
 
         for objectToProcess in range(0, len(numPieces)):
             for index in range(0,numPieces[objectToProcess]):
@@ -156,4 +158,4 @@ def processMesh(nodes, nodePieces, numPieces):
                     hou_keyframe.setValue(hou.Quaternion(nodes[objectToProcess].simulation().findObject(nodes[objectToProcess].name()).geometry().iterPoints()[index].attribValue('orient')).extractEulerRotates()[index_parm])
                     hou_keyed_parm.setKeyframe(hou_keyframe)
 
-    print "Processing Complete!"
+    print 'Processing Complete!'

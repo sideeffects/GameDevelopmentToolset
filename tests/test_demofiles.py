4d2b8c97c956426da26e8b20d5a1faddcb83a6ec
DESKTOP-P98C1VA\MAINGEAR <lkruel@gmail.com>
2018-08-17 17:28:20 -0400

    fixing error on remote running of unit test

--- tests/test_demofiles.py
+++ tests/test_demofiles.py
@@ -13,7 +13,7 @@ class TestStringMethods(unittest.TestCase):
 
     def test_1_test_demoscenes(self):
 
-        demo_files = os.listdir("../hip")
+        demo_files = os.listdir(os.path.dirname(local_dir) + "/hip")
         for demo_file in demo_files:
             if demo_file.endswith(".hip"):
                 print "opening", demo_file

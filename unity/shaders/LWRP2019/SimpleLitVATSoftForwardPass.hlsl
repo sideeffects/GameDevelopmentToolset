9e6f628265101b2331ea52efd5f7e53240a826b3
Mike Lyndon <mikel@sidefx.com>
2019-05-14 11:14:29 -0400

    [UPDATE] Unity LWRP shaders have been updated for Unity 2019

---
+++ unity/shaders/LWRP2019/SimpleLitVATSoftForwardPass.hlsl
@@ -0,0 +1,178 @@
+#ifndef LIGHTWEIGHT_SIMPLE_LIT_VAT_FLUID_PASS_INCLUDED
+#define LIGHTWEIGHT_SIMPLE_LIT_VAT_FLUID_PASS_INCLUDED
+
+#include "Packages/com.unity.render-pipelines.lightweight/ShaderLibrary/Lighting.hlsl"
+
+struct Attributes
+{
+    float4 positionOS    : POSITION;
+    float3 normalOS      : NORMAL;
+    float4 tangentOS     : TANGENT;
+    float4 texcoord      : TEXCOORD0;
+    float4 texcoord1     : TEXCOORD1;
+    float2 lightmapUV    : TEXCOORD2;
+    float4 color         : COLOR;
+    UNITY_VERTEX_INPUT_INSTANCE_ID
+};
+
+struct Varyings
+{
+    float4 uv                       : TEXCOORD0;
+    float4 custom                   : TEXCOORD1;
+    DECLARE_LIGHTMAP_OR_SH(lightmapUV, vertexSH, 2);
+
+    float4 posWSShininess           : TEXCOORD3;    // xyz: posWS, w: Shininess * 128
+
+#ifdef _NORMALMAP
+    half4 normal                    : TEXCOORD4;    // xyz: normal, w: viewDir.x
+    half4 tangent                   : TEXCOORD5;    // xyz: tangent, w: viewDir.y
+    half4 bitangent                 : TEXCOORD6;    // xyz: bitangent, w: viewDir.z
+#else
+    half3  normal                   : TEXCOORD4;
+    half3 viewDir                   : TEXCOORD5;
+#endif
+
+    half4 fogFactorAndVertexLight   : TEXCOORD7; // x: fogFactor, yzw: vertex light
+
+#ifdef _MAIN_LIGHT_SHADOWS
+    float4 shadowCoord              : TEXCOORD8;
+#endif
+    float4 color                    : COLOR;
+
+    float4 positionCS               : SV_POSITION;
+    UNITY_VERTEX_INPUT_INSTANCE_ID
+    UNITY_VERTEX_OUTPUT_STEREO
+};
+
+
+void InitializeInputData(Varyings input, half3 normalTS, out InputData inputData)
+{
+    inputData.positionWS = input.posWSShininess.xyz;
+
+#ifdef _NORMALMAP
+    half3 viewDirWS = half3(input.normal.w, input.tangent.w, input.bitangent.w);
+    inputData.normalWS = TransformTangentToWorld(normalTS,
+        half3x3(input.tangent.xyz, input.bitangent.xyz, input.normal.xyz));
+#else
+    half3 viewDirWS = input.viewDir;
+    inputData.normalWS = input.normal;
+#endif
+
+#if SHADER_HINT_NICE_QUALITY
+    viewDirWS = SafeNormalize(viewDirWS);
+#endif
+
+    inputData.normalWS = NormalizeNormalPerPixel(inputData.normalWS);
+
+    inputData.viewDirectionWS = viewDirWS;
+#if defined(_MAIN_LIGHT_SHADOWS) && !defined(_RECEIVE_SHADOWS_OFF)
+    inputData.shadowCoord = input.shadowCoord;
+#else
+    inputData.shadowCoord = float4(0, 0, 0, 0);
+#endif
+    inputData.fogCoord = input.fogFactorAndVertexLight.x;
+    inputData.vertexLighting = input.fogFactorAndVertexLight.yzw;
+    inputData.bakedGI = SAMPLE_GI(input.lightmapUV, input.vertexSH, inputData.normalWS);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//                  Vertex and Fragment functions                            //
+///////////////////////////////////////////////////////////////////////////////
+
+// Used in Standard (Simple Lighting) shader
+Varyings LitPassVertexSimple(Attributes input)
+{
+    Varyings output = (Varyings)0;
+
+    UNITY_SETUP_INSTANCE_ID(input);
+    UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(output);
+
+    //Fetch vertex position from pos and rot VAT textures
+    VATAttributes outputVAT = GetAttributesFromTexture(TEXTURE2D_ARGS(_posTex, sampler_posTex), 
+                    TEXTURE2D_ARGS(_nTex, sampler_nTex), TEXTURE2D_ARGS(_colorTex, sampler_colorTex),
+                    input.texcoord, input.texcoord1, input.color, input.positionOS, input.normalOS);
+
+    VertexPositionInputs vertexInput = GetVertexPositionInputs(outputVAT.positionVAT.xyz);
+    
+    // I'm not modifying the tangents in the GetVertexPositionInputs function which
+    // might be a problem...
+
+    VertexNormalInputs normalInput = GetVertexNormalInputs(outputVAT.normalVAT, input.tangentOS);
+    half3 viewDirWS = GetCameraPositionWS() - vertexInput.positionWS;
+
+#if !SHADER_HINT_NICE_QUALITY
+    viewDirWS = SafeNormalize(viewDirWS);
+#endif
+
+    half3 vertexLight = VertexLighting(vertexInput.positionWS, normalInput.normalWS);
+    half fogFactor = ComputeFogFactor(vertexInput.positionCS.z);
+
+    output.uv.xy = TRANSFORM_TEX(input.texcoord, _BaseMap);
+    output.custom.xy = input.texcoord1.zw;
+
+    output.posWSShininess.xyz = vertexInput.positionWS;
+    output.posWSShininess.w = _Shininess * 128.0;
+    output.positionCS = vertexInput.positionCS;
+
+#ifdef _NORMALMAP
+    output.normal = half4(normalInput.normalWS, viewDirWS.x);
+    output.tangent = half4(normalInput.tangentWS, viewDirWS.y);
+    output.bitangent = half4(normalInput.bitangentWS, viewDirWS.z);
+#else
+    output.normal = normalInput.normalWS;
+    output.viewDir = viewDirWS;
+#endif
+    output.color = outputVAT.colorVAT;
+
+    OUTPUT_LIGHTMAP_UV(input.lightmapUV, unity_LightmapST, output.lightmapUV);
+    OUTPUT_SH(output.normal.xyz, output.vertexSH);
+
+    output.fogFactorAndVertexLight = half4(fogFactor, vertexLight);
+
+#if defined(_MAIN_LIGHT_SHADOWS) && !defined(_RECEIVE_SHADOWS_OFF)
+    output.shadowCoord = GetShadowCoord(vertexInput);
+#endif
+
+    return output;
+}
+
+// Used for StandardSimpleLighting shader
+half4 LitPassFragmentSimple(Varyings input) : SV_Target
+{
+    UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
+
+    float2 uv = input.uv;
+    half4 diffuseAlpha = SampleAlbedoAlpha(uv, TEXTURE2D_ARGS(_BaseMap, sampler_BaseMap));
+    half3 diffuse = diffuseAlpha.rgb * _BaseColor.rgb * input.color.rgb;
+    // diffuse = input.color;
+    half alpha = diffuseAlpha.a * _BaseColor.a;
+
+#ifdef _ALPHAPREMULTIPLY_ON
+    diffuse *= alpha;
+#endif
+
+    half3 normalTS = SampleNormal(uv, TEXTURE2D_ARGS(_BumpMap, sampler_BumpMap));
+    half3 emission = SampleEmission(uv, _EmissionColor.rgb, TEXTURE2D_ARGS(_EmissionMap, sampler_EmissionMap));
+    // float seed = input.color.b;
+    // float bbox = input.color.a;
+    // seed = seed * 0.2 + 0.8;
+    // float rampPos = input.color.r + input.color.g;
+    // rampPos *= bbox;
+    // rampPos *= seed;
+    // rampPos /= 1.3;
+    // rampPos += input.custom.y;
+    // rampPos = clamp(rampPos, 0.01, 0.99);
+    // emission = SampleRamp(rampPos, TEXTURE2D_PARAM(_RampTex, sampler_RampTex));
+    // emission = input.color;
+    half4 specular = SampleSpecularSmoothness(uv, diffuseAlpha.a, _SpecColor, TEXTURE2D_ARGS(_SpecGlossMap, sampler_SpecGlossMap));
+    half shininess = input.posWSShininess.w;
+
+    InputData inputData;
+    InitializeInputData(input, normalTS, inputData);
+
+    half4 color = LightweightFragmentBlinnPhong(inputData, diffuse, specular, shininess, emission, alpha);
+    color.rgb = MixFog(color.rgb, inputData.fogCoord);
+    return color;
+};
+
+#endif
